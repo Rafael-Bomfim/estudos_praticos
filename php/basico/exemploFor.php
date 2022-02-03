181df071db97c2697bf77d50058d6d8a8186215e
Rafael Bomfim <rafinhapbomfim@gmail.com>
2022-02-02 22:36:02 -0300

    Estudos iniciantes de PHP com MySQL

---
+++ php/basico/exemploFor.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Soma dos números \n";
+$soma = 0;
+
+for($i = 0; $i <= 20; $i ++) {
+    $soma = $soma + $i;
+}
+echo $soma ."\n";
+
+echo "______________________________ \n";
+
+echo "Tabuada de um número \n";
+
+$numTabuada = 6;
+
+for($i = 0; $i <= 10; $i ++) {
+    echo $i = $numTabuada . "\n";
+}
+
+?>
---
+++ php/basico/exemploSwitchCase.php
@@ -0,0 +1,17 @@
+<?php
+
+$num = 5;
+
+switch ($num) { // só testa variáveis qu possam ser convertidas para inteiro
+    case 10:       //blocos cases
+        echo "O valor é 10";
+        break;
+    case 5:
+        echo "O valor é 5";
+        break;
+    case 0:
+        echo "Ovalor é 0";
+        break;
+}
+
+?>
